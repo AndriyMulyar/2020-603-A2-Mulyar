5b596a5c9705f2e2b469f1528802cc31c410bd5a
Andriy Mulyar <andriy.mulyar@gmail.com>
2020-09-29 14:08:57 -0400

    Initial commit

---
+++ main.cu
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cuda_runtime.h>
+using namespace std;
+int main() {
+	int deviceCount = 0;
+	cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
+	if (error_id != cudaSuccess) {
+		printf("cudaGetDeviceCount returned %d\n-> %s\n",
+				static_cast<int>(error_id), cudaGetErrorString(error_id));
+		printf("Result = FAIL\n");
+		exit(EXIT_FAILURE);
+	}
+// This function call returns 0 if there are no CUDA capable devices.
+	if (deviceCount == 0) {
+		printf("There are no available device(s) that support CUDA\n");
+	} else {
+		printf("Detected %d CUDA Capable device(s)\n", deviceCount);
+	}
+	return 0;
+}
